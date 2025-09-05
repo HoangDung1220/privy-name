2eca8407f12bc633cd54a713e76fee9116da0f7b
“narutoLab” <naruto07061998@gmail.com>
2025-09-05 22:02:51 +0700

    create header ui

--- packages/site/abi/FHECounterAddresses.ts
+++ packages/site/abi/FHECounterAddresses.ts
@@ -4,6 +4,6 @@
   Command: 'npm run genabi'
 */
 export const FHECounterAddresses = { 
-  "11155111": { address: "0xd96Bc116AD7772422eF344e55a7763159b6f5953", chainId: 11155111, chainName: "sepolia" },
+  "11155111": { address: "0x208862bDA9f9A00A30d0eFD05BEDE863E8CC9Fbd", chainId: 11155111, chainName: "sepolia" },
   "31337": { address: "0x7553CB9124f974Ee475E5cE45482F90d5B6076BC", chainId: 31337, chainName: "hardhat" },
 };
